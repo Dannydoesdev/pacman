aa99102703c470d58f2343b91a1451312dec1b06
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-02-28 22:40:27 +1100

    Cleanup logs, added extra boundaries

--- pacman.js
+++ pacman.js
@@ -40,27 +40,26 @@
 
 const pacManGame = (commands) => {
 
+  if (!commands) { console.log('No commands receive'); return }
+  
   let started = false;
-  let coordsX = 0;
-  let coordsY = 0;
+  let coordsX = null;
+  let coordsY = null;
   let direction = '';
   let compass = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
-  let moves = ['LEFT', 'RIGHT'];
-
+  let output = '';
 
   commands.map((command) => {
-    console.log(command)
     // check for PLACE command & start game - if already stared, restart
     if (command.startsWith('PLACE')) {
 
       if (started) {
         console.log(`Restarting at ${command}`);
-        // return
+      } else {
+        // Start game
+        console.log('Starting Game!')
+        started = true;
       }
-
-      // Start game
-      console.log('Starting Game!')
-      started = true;
 
       // remove the word PLACE & split by comma
       let placeCoords = command.slice(6).split(',')
@@ -92,26 +91,17 @@ const pacManGame = (commands) => {
 
     if (command == 'RIGHT') {
 
-      console.log('Turning right')
-      console.log(`Initial index ${compass.indexOf(direction)}`)
       let newDirectionIndex = (compass.indexOf(direction) + 1)
-      console.log(`New index = ${newDirectionIndex}`)
 
       // Check for directions going outside of compass array & reset
       if (newDirectionIndex == 4) { newDirectionIndex = 0 };
 
       let newDirection = compass[newDirectionIndex];
       direction = newDirection;
-      console.log(`New direction ${newDirection}`);
 
     } else if ((command == 'LEFT')) {
 
-      console.log('Turning left')
-      console.log(`Initial index ${compass.indexOf(direction)}`)
-
       let newDirectionIndex = (compass.indexOf(direction) - 1)
-
-      console.log(`New index = ${newDirectionIndex}`)
 
       // Check for directions going outside of compass array & reset
       if (newDirectionIndex == -1){newDirectionIndex = 3}
@@ -119,74 +109,74 @@ const pacManGame = (commands) => {
       // Update direction to newDirection (based on incremented index)
       let newDirection = compass[newDirectionIndex];
       direction = newDirection;
-
-      console.log(`New direction ${newDirection}`);
     }
 
     if (command == 'MOVE') {
       if (direction == 'NORTH') {
-        console.log(`Moving North - current co-ordinates (${coordsX},${coordsY})`)
+
         coordsY >= 4 ? console.log('Pacman cant go further that way') : coordsY++;
-        console.log(`Moved North - new co-ordinates (${coordsX},${coordsY})`)
+
       } else if (direction == 'EAST') {
 
-        console.log(`Moving East - current co-ordinates (${coordsX},${coordsY})`)
         coordsX >= 4 ? console.log('Pacman cant go further that way') : coordsX++;
-        console.log(`Moved East - new co-ordinates (${coordsX},${coordsY})`)
 
       } else if (direction == 'SOUTH') {
 
-        console.log(`Moving South - current co-ordinates (${coordsX},${coordsY})`)
         coordsY <= 0 ? console.log('Pacman cant go further that way') : coordsY--;
-        console.log(`Moved South - new co-ordinates (${coordsX},${coordsY})`)
 
       } else if (direction == 'WEST') {
 
-        console.log(`Moving West - current co-ordinates (${coordsX},${coordsY})`)
         coordsX <= 0 ? console.log('Pacman cant go further that way') : coordsX--;
-        console.log(`Moved West - new co-ordinates (${coordsX},${coordsY})`)
-
       }
-
     }
 
     if (command == 'REPORT') {
-      console.log(`Output: ${coordsX},${coordsY},${direction}`)
+      if (coordsX === null || coordsY === null) {
+        console.log('no valid placement - resetting output')
+        output = '';
+      } else {
+        output = `${coordsX},${coordsY},${direction}`;
+      }
     }
-
+  
   })
-  // console.log(`Ending at (${coordsX},${coordsY}) facing ${direction}`)
+  console.log(`Output: ${output}`)
+  return output
 
 }
 
-// NOTES
-// Extra Place commands are allowed:
-// The first valid command to Pacman is a PLACE command, after that, any sequence of commands may be issued, in any order, including another PLACE command.
 
-// PLACE must check for invalid coordinates
-// Pacman must not move off the grid during movement. This also includes the initial placement of Pacman.
-
-let testCommands1 = ['MOVE', 'PLACE 0,0,NORTH', 'MOVE', 'REPORT']
-let testCommands2 = ['MOVE', 'PLACE 0,0,NORTH', 'LEFT', 'REPORT']
-let testCommands3 = ['MOVE', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT']
-let testCommands4 = ['MOVE', 'PLACE 1,2,EAST', 'RIGHT', 'RIGHT', 'LEFT', 'RIGHT', 'LEFT', 'REPORT']
-let testCommands5 = ['MOVE', 'PLACE 1,2,EAST', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'PLACE 3,2,NORTH', 'RIGHT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'REPORT']
+let testCommands1 = ['MOVE', 'PLACE 0,0,NORTH', 'MOVE', 'REPORT'];
+let testCommands2 = ['MOVE', 'PLACE 0,0,NORTH', 'LEFT', 'REPORT'];
+let testCommands3 = ['MOVE', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT'];
+let testCommands4 = ['MOVE', 'PLACE 1,2,EAST', 'RIGHT', 'RIGHT', 'LEFT', 'RIGHT', 'LEFT', 'REPORT'];
+let testCommands5 = ['MOVE', 'PLACE 1,2,EAST', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'PLACE 3,2,NORTH', 'RIGHT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'REPORT'];
 let testCommands6 = ['MOVE', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'MOVE', 'MOVE',
-  'MOVE', 'LEFT', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'REPORT']
-  let testCommands7 = ['MOVE', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'MOVE', 'MOVE',
-  'MOVE', 'LEFT', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'REPORT', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT']
+  'MOVE', 'LEFT', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'MOVE', 'REPORT'];
+let testCommands7 = ['MOVE', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'MOVE', 'MOVE',
+  'MOVE', 'LEFT', 'MOVE', 'MOVE', 'PLACE 6,9,EAST', 'MOVE', 'MOVE', 'MOVE', 'REPORT', 'PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT'];
+let testCommands8 = ['PLACE 0,0,EAST', 'MOVE', 'PLACE 0,0,NORTH', 'MOVE', 'REPORT'];
+let testCommands9 = ['PLACE -1,0,NORTH', 'MOVE', 'REPORT'];
+let testCommands10 = ['MOVE', 'PLACE 0,1,EAST', 'MOVE', 'MOVE', 'MOVE', 'MOVE',
+'MOVE', 'LEFT', 'MOVE', 'MOVE', 'PLACE 6,9,EAST', 'MOVE', 'MOVE', 'MOVE', 'REPORT']
 
-// console.log('\n---GAME 1---\n')
-// pacManGame(testCommands1)
-// console.log('\n---GAME 2---\n')
-// pacManGame(testCommands2)
-// console.log('\n---GAME 3---\n')
-// pacManGame(testCommands3)
-// console.log('\n---GAME 4---\n')
-// pacManGame(testCommands4)
-// console.log('\n---GAME 5---\n')
-// pacManGame(testCommands5)
-// console.log('\n---GAME 6---\n')
-// pacManGame(testCommands6)
+console.log('\n---GAME 1---\n')
+pacManGame(testCommands1)
+console.log('\n---GAME 2---\n')
+pacManGame(testCommands2)
+console.log('\n---GAME 3---\n')
+pacManGame(testCommands3)
+console.log('\n---GAME 4---\n')
+pacManGame(testCommands4)
+console.log('\n---GAME 5---\n')
+pacManGame(testCommands5)
+console.log('\n---GAME 6---\n')
+pacManGame(testCommands6)
 console.log('\n---GAME 7---\n')
 pacManGame(testCommands7)
+console.log('\n---GAME 8---\n')
+pacManGame(testCommands8)
+console.log('\n---GAME 9---\n')
+pacManGame(testCommands9)
+console.log('\n---GAME 10---\n')
+pacManGame(testCommands10)
